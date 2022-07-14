b70cd59523af097131ee517f392d0a5021eeac22
Danko Valderrama <danko.h224@gmail.com>
2022-07-14 05:47:00 -0400

    estudio de diccionarios

--- 20 diccionarios/diccionarios.py
+++ 20 diccionarios/diccionarios.py
@@ -1,26 +1,48 @@
-# Un dicionario es otro tipo de forma de guardar datos. Tenemos las listas, las tuplas y los diccionarios. El diccionario se compone de elementos que a su vez se deviden en clave e información que asociar a esa clave. Veamos el siguiente ejemplo de diccionario de telefonos.
-telefonos = {
-    "jose": 1234,
-    "maría": 2142,
-    "andrés": 3242,
-    "lucía": 3432
-}
+# # Un dicionario es otro tipo de forma de guardar datos. Tenemos las listas, las tuplas y los diccionarios. El diccionario se compone de elementos que a su vez se deviden en clave e información que asociar a esa clave. Veamos el siguiente ejemplo de diccionario de telefonos.
+# telefonos = {
+#     "jose": 1234,
+#     "maría": 2142,
+#     "andrés": 3242,
+#     "lucía": 3432
+# }
 
-# Consultar un valor asociado a una clave :
-print(telefonos["maría"])
+# # Consultar un valor asociado a una clave :
+# print(telefonos["maría"])
 
-# Agregar un elemento:
-telefonos["jorge"] = 4543
-print(telefonos["jorge"])
+# # Agregar un elemento:
+# telefonos["jorge"] = 4543
+# print(telefonos["jorge"])
 
-# Modificar elementos:
-telefonos["jorge"] = 4732
-print(telefonos["jorge"])
+# # Modificar elementos:
+# telefonos["jorge"] = 4732
+# print(telefonos["jorge"])
 
-# Eliminar elementos:
-del telefonos["jorge"]
-print(telefonos)
+# # Eliminar elementos:
+# del telefonos["jorge"]
+# print(telefonos)
 
-# Eliminar todo el diccionario:
-del telefonos 
-print(telefonos)
+# # Eliminar todo el diccionario:
+# del telefonos
+
+
+# # RECORRER BUCLE FOR:
+# # lista nueva:
+d = {"animal": "perro","color":"blanco","edad":4}
+# print(d.keys())
+
+# # Recorrer las claves del diccionario
+# for k in d:
+#     print(k)
+
+
+# # Recorrer los valores del diccionario
+for k in d:
+    print(d[k])
+
+# Recorrer los valores del diccionario
+# for v in d.values():
+#     print(v)
+
+# # Recorrer los pares clave valor
+# for i in d.items():
+#     print(i)
---
+++ 20 diccionarios/metodos.py
@@ -0,0 +1,20 @@
+# METODOS: 
+# print(dir(dict))
+
+
+d = {"animal": "perro","color":"blanco","edad":4}
+
+
+# Metodo GET: se usa para no tener que hacer un if & else para corregir el error de que no se encuentre la clave. Primer argumento indica la clave, segundo argumento entrega un mensaje de error.
+# print(d.get("animal","Esta clave no existe."))
+
+# Este método puede servir por ejemplo para hallar las unidades de ciertos articulos de un inventario:
+inventario = {
+    "tornillos": 100,
+    "tuercas": 12,
+    "arandelas": 323,
+}
+
+articulo = str(input("Ingrese nombre articulo: "))
+cantidad = inventario.get(articulo,0)
+print("{}: {}".format(articulo,cantidad))
